b7a7c57b52d9599223ba9482ca7ab8ec85b2b366
carlesViolan <carlesviolan@gmail.com>
2024-07-31 16:17:21 +0000

    Carles_Violan_InstagramData

--- src/models.py
+++ src/models.py
@@ -1,31 +1,81 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Table, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+# Association table
+
+follow = Table(
+    'follow', Base.metadata,
+    Column('following_id', Integer, ForeignKey('user.id')),
+    Column('follower_id', Integer, ForeignKey('user.id'))
+)
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    followers = Column(Integer, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+    post = relationship('Post', backref="user")
+    followers = relationship('User', 
+                                secondary = follow, 
+                                primaryjoin = (follow.c.following_id == id),
+                                secondaryjoin = (follow.c.follower_id == id),
+                                backref = 'following'
+                                )    
+# followers = User.relationship('User', 
+#                      secondary = followe, 
+#                      primaryjoin = (follow.c.following_id == id),
+#                      secondaryjoin = (follow.c.follower_id == id),
+#                     backref = 'following'
+#                                 )       
+           
+                 
+
+class Comment(Base):
+    __tablename__ = 'comment'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    comment = Column(String(250))
+    
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
-    def to_dict(self):
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)   
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    type = Column(Integer, primary_key=True)
+    url = Column(String(250), nullable=False)
+
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+                        
+
+
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
